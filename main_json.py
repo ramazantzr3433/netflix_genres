e85c215938a4eae3a3128a20fe113e5c35e0695a
ramazan <ramazantzr@hotmail.com>
2021-05-04 17:24:37 +0300

    -Veri tabanına gerek kalmadan json ile verileri cekme

---
+++ main_json.py
@@ -0,0 +1,43 @@
+from bson.json_util import dumps, loads
+from flask import Flask
+from werkzeug.utils import secure_filename
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello_world():
+    return dumps({'status': True, 'msg': 'Merhaba :)'})
+
+
+@app.route('/get_genres/', methods=['GET'])
+def get_genres():
+    with open('genres.json', 'r', encoding='utf-8') as file:
+        all_genres = loads(file.read())
+        for a in all_genres:
+            a['name'] = secure_filename(a.get('name'))
+    if all_genres is not None:
+        return dumps({'status': True, 'data': all_genres})
+    else:
+        return dumps({'status': False, 'err': "Netflix kategorilerini çekemiyorum, üzgünüm :("})
+
+
+@app.route('/get_genre/<_id>', methods=['GET'])
+def get_genre(_id):
+    name = _id
+    print(name)
+    with open('genres.json', 'r', encoding='utf-8') as file:
+        all_genres = loads(file.read())
+        for gn in all_genres:
+            gn['name'] = secure_filename(gn.get('name'))
+            if str(gn.get('name')) == name:
+                genre = gn
+
+    if genre is not None:
+        return dumps({'status': True, 'data': genre})
+    else:
+        return dumps({'status': False, 'err': "Netflix kategorisini çekemiyorum, üzgünüm :("})
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
